2e869e2d76220c6e585e5e2fbf7b4a4aaa636d7a
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-15 10:28:56 -0800

    Change fonts and styling

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let canvas = document.getElementById('canvas');
   let game = new Game(canvas)
 
-  document.getElementById('title').innerHTML = `Drifting Simulator`;
-  document.getElementById('description').innerHTML = `Use WASD to move your car around. Drift and score as many points as possible in 60 seconds. You will lose points if you hit any Obstacles!`;
+  document.getElementById('title').innerHTML = `Drift Car Simulator`;
+  document.getElementById('description').innerHTML = `Use WASD to move your car around. Drift and score as many points as possible within 60 seconds. You will lose points if you hit any obstacles!`;
   document.getElementById('play').innerHTML = 'Play';
 
   document.getElementById('play').addEventListener('click', (e) => {
